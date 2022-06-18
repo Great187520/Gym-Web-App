1d1d1860059b340edf6f56984041df52ef6a7e43
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-18 16:36:42 +0100

    Add files via upload

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,6 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        //'X-RapidAPI-Key': '62e3c65b68mshdb3378d32bfe5a2p19e631jsn6e7f7de52363',
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     }
       
@@ -12,7 +11,7 @@ export const youtubeOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': '',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     },
   };
 
